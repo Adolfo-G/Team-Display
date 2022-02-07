cea8610a4a74ff52df3cd0ca9d7ee7cd117d3578
Adolfo Gonzalez <agonzal177@gmail.com>
2022-02-07 12:26:58 -0500

    added classes and properties for employee, intern, engineer, and manager. was able to retrieve object data from intern class

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
-const inquirer=request("inquirer")
-const generate=require("generateHtml.js")
+const inquirer=require("inquirer")
+const Intern=require("./lib/intern")
+const Engineer=require("./lib/engineer")
+const Manager=require("./lib/manager")
 const fs =require("fs")
+let team=[]
 
 //prompts
+function generateIntern(){
+const internData= () => {
+        return inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "Please enter this persons name."
+          },
+          {
+            type: "input",
+            name: "id",
+            message: "Please enter this persons ID."
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Please enter this persons email."
+          },
+          {
+            type: "input",
+            name: "school",
+            message: "Please enter this persons school."
+          },
+        ])
+      }
+    
+internData()
+    .then((data)=>{
+        const intern= new Intern(data.name,data.id,data.email,data.school);
+        console.log(intern)
+        team.push(intern)
+    });
+}
+
+generateIntern()
+    // .then(()=>generateIntern())
+    // .then(console.log(team))
 
 //writeFile
 
